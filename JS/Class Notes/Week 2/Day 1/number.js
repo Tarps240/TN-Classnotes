0813b287b15d9179f3938b75a19a6dec482478a4
Tarps240 <sl3966@gmail.com>
2024-07-29 09:16:13 -0500

    JS Week 2 day 1 start

---
+++ JS/Class Notes/Week 2/Day 1/number.js
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////Welcome To Numbers///////////////////////////////
+///////////////////////////////////////////primitive data type/////////////////////////////
+
+//Declaring Number Data Types
+
+
+// More Examples
+
+
+//////When using numbers we have object methods that allow as to do maths with our number data types. Here is a list of examples of different Math method. If you want to understand when to use each then look in w3schools. 
+
+
+
+
+// Rounding to the closest number
+   
+
+
+// Let us  create random number between 0 to 10
+
+
+
+//Absolute value
+
+//Square root
+
+
+// Power
+
+
+// Logarithm
+// Returns the natural logarithm with base E of x, Math.log(x)
+
+
+// Returns the natural logarithm of 2 and 10 respectively
+
+
+// Trigonometry
---
+++ JS/Class Notes/Week 2/Day 1/string.js
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////Welcome To String///////////////////////////////
+///////////////////////////////////////////primitive data type/////////////////////////////
+
+//three different kinds of strings 
+
+
+//concatinating 
+
+
+
+///////////////////Escape sequence characters////////////////////////
+
+//     \n: new line
+//     \t: Tab, means 8 spaces
+//     \\: Back slash
+//     \': Single quote (')
+//     \": Double quote (")
+
+
+
+///////////////////////////////////String Methods ///////////////////////////////
+
+
+
+
+///////accsesing the character P in my JS string 
+//The way to accses characters in my string is using the index value. 
+//ie:   "JavaScript"
+/////////0123456789///////
+
+
+
+
+
+////ascr
+let js="Javascript";
+////////0123456789///
+
