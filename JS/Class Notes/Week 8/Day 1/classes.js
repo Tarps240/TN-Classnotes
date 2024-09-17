fbd8864afa53b294d05476108fa057519e7a7160
Tarps240 <sl3966@gmail.com>
2024-09-17 07:18:55 -0700

    Week 8 update

---
+++ JS/Class Notes/Week 8/Day 1/classes.js
@@ -0,0 +1,31 @@
+// Classes
+    // Defining a classes
+
+    
+
+    // Class Instantiation
+
+
+    // Class Constructor
+
+
+    // Default values with constructor
+
+
+    // Class methods
+
+
+    // Properties with initial value
+
+
+    // getter
+
+
+    // setter
+
+
+    // Static method
+
+    
+// Inheritance
+    // Overriding methods
