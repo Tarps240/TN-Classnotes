885d382bca9e3879f63334ea1e3bd2426d80fcd8
Tarps240 <sl3966@gmail.com>
2024-09-18 21:28:10 -0700

    Classes activity update.

--- JS/Class Notes/Week 8/Day 1/activity.js
+++ JS/Class Notes/Week 8/Day 1/activity.js
@@ -1,23 +1,82 @@
 
 // Create an Animal class. The class will have name, age, color, legs properties and create different methods
 
+class Animal {
+        constructor(name, age, color, legs) {
+            this.name = name;
+            this.age = age;
+            this.color = color;
+            this.legs = legs;
+        }
+    
+        describe() {
+            return `This is a ${this.age}-year-old ${this.color} animal named ${this.name} with ${this.legs} legs.`;
+        }
+    
+        speak() {
+            return `${this.name} makes a noise.`;
+        }
 
+        celebrateBirthday() {
+                this.age += 1;
+                return `${this.name} just turned ${this.age} years old!`;
+        }
 
+        getLegs() {
+                return `${this.name} has ${this.legs} legs.`;
+        }
 
+ }
+    
 // Create a Dog and Cat child class from the Animal Class.
 
+const dog = new Animal('Luna', 2, 'White', 4);
+const cat = new Animal('Salem', 5, 'Black', 4);
+
+// Print the objects created.
+console.log(dog);
+console.log(cat);
+
+// Calling methods
+console.log(cat.describe());
+console.log(dog.speak());
+console.log(dog.celebrateBirthday());
+console.log(cat.getLegs());
 
 
 
+// Override the method you created in the Animal class.
+// You can override a method in a class by redefining it in a subclass.
 
-// Override the method you create in Animal class
+class Dog extends Animal {
+    constructor(name, age, color, legs, breed) {
+        super(name, age, color, legs); // Call the parent class constructor
+        this.breed = breed;            // Additional property specific to Dog
+    }
+
+    // Overriding the speak method
+    speak() {
+        return `${this.name} barks loudly!`;
+    }
+}
+
+// Creating an instance of the Dog class
+const myDog = new Dog('Buddy', 3, 'brown', 4, 'Labrador');
+
+// Calling the overridden speak method
+console.log(myDog.speak()); 
+
+// Calling the inherited describe method
+console.log(myDog.describe());
 
 
 
-
-
-// Let's try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and measure of variability(range, variance, standard deviation). In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample. You can create a class called Statistics and create all the functions which do statistical calculations as method for the Statistics class. Check the output below.
+// Let's try to develop a program which calculates the measure of central tendency of a sample (mean, median, mode) and the measure of variability (range, variance, standard deviation). In addition to those measures, find the min, max, count, percentile, and frequency distribution of the sample. You can create a class called Statistics and create all the functions which do statistical calculations as methods for the Statistics class. Check the output below.
 // ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+
+
+
+
 
 // console.log('Count:', statistics.count()) // 25
 // console.log('Sum: ', statistics.sum()) // 744
@@ -51,3 +110,60 @@
 
 
 // Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+
+class PersonAccount {
+    constructor(firstname, lastname) {
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.incomes = []; // Array to hold income objects {amount, description}
+      this.expenses = []; // Array to hold expense objects {amount, description}
+    }
+  
+    // Method to calculate total income
+    totalIncome() {
+      return this.incomes.reduce((total, income) => total + income.amount, 0);
+    }
+  
+    // Method to calculate total expenses
+    totalExpense() {
+      return this.expenses.reduce((total, expense) => total + expense.amount, 0);
+    }
+  
+    // Method to return account information
+    accountInfo() {
+      return `Account Holder: ${this.firstname} ${this.lastname}, Total Income: $${this.totalIncome()}, Total Expenses: $${this.totalExpense()}`;
+    }
+  
+    // Method to add income
+    addIncome(amount, description) {
+      this.incomes.push({ amount, description });
+    }
+  
+    // Method to add expense
+    addExpense(amount, description) {
+      this.expenses.push({ amount, description });
+    }
+  
+    // Method to calculate the account balance
+    accountBalance() {
+      return this.totalIncome() - this.totalExpense();
+    }
+  }
+  
+  // Example usage:
+  const account = new PersonAccount("Scott", "Lewis");
+  
+  // Adding incomes
+  account.addIncome(2000, "Salary");
+  account.addIncome(500, "Freelance");
+  
+  // Adding expenses
+  account.addExpense(300, "Rent");
+  account.addExpense(200, "Groceries");
+  
+  // Display account info
+  console.log(account.accountInfo()); 
+  
+  // Display account balance
+  console.log(`Account Balance: $${account.accountBalance()}`); 
+  
--- JS/Class Notes/Week 8/Day 1/classes.js
+++ JS/Class Notes/Week 8/Day 1/classes.js
@@ -1,31 +1,76 @@
+// const car1 = {
+//     make: "Honda",
+//     model: "Accord",
+//     year: 1998,
+//     color: "Taupe",
+//     awd: false
+// };
+
+// const car2 = {
+//   make: "Mercedes",
+//   model: "CLS55",
+//   year: 2006,
+//   color: "Black",
+//   awd: true,
+// };
+
 // Classes
-    // Defining a classes
+// Defining a class
 
-    
+class car {
+  // Class Constructor
+  // Default values with constructor
+  constructor(make = "Honda", model = "CRV", year = 2024, color = "Blue", awd = true, inventory = 0) {
+    (this.make = make),
+      (this.model = model),
+      (this.year = year),
+      (this.color = color),
+      (this.awd = awd),
+      (this.inventory = 0);
+  }
 
-    // Class Instantiation
+// Class methods
+// getSpeed (terrain) {
+//   if (terrain == "city") {
+//     return `25 Miles per gallon.`
+//   } else if (terrain == "highway") {
+//     return `40 Miles per gallon.`
+//   }
+// };
 
 
-    // Class Constructor
+// getter
+get getInventory () {
+  return this.inventory
+}
 
 
-    // Default values with constructor
+// setter
+set setInventory (newInventory) {
+  this.inventory += newInventory;
+};
+
+}
+
+// Class Instantiation
+
+const car1 = new car("Honda", "Civic", 1999, "Yellow", false, 4);
+const car2 = new car("Ford", "Fusion", 2002, "Green", true, 3);
+const car3 = new car("Mercedes", "CLS55", 2006, "Black", false, 10);
+const car4 = new car();
+
+car1.setInventory = 5;
+car2.setInventory = 2;
+car3.setInventory = 9;
 
 
-    // Class methods
+console.log(car1);
+console.log(car2);
+console.log(car3);
+console.log(car4);
 
-
-    // Properties with initial value
-
-
-    // getter
-
-
-    // setter
-
-
-    // Static method
-
-    
 // Inheritance
-    // Overriding methods
+
+
+
+// Overriding methods
