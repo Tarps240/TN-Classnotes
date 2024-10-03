71657b55c847a6eb07982776f72f3cc929d16e60
Tarps240 <sl3966@gmail.com>
2024-10-03 06:52:36 -0700

    Thursday Morning Update

--- Final HTML CSS Project/js/main.js
+++
@@ -1,6 +0,0 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const menu = document.querySelector('.menu');
-
-mobileMenu.addEventListener('click', () => {
-    menu.classList.toggle('open');
-});
