3dff104a132092f01e6d1538d9d5293699cfe46f
Tarps240 <sl3966@gmail.com>
2024-10-26 06:46:35 -0500

    Fixed ColorBar assignment

--- React/Assignments/jsx-components/src/App.jsx
+++ React/Assignments/jsx-components/src/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import ColorBar from './ColorBar';
-import './App.css';
+import ReactDOM from 'react-dom';
+import './App.css'
+
+const rootElement = document.getElementById('root');
+
+const ColorBar = ({ color }) => {
+    return (
+        <div className = 'color-bar' style={{ backgroundColor: color }}>
+            {color}
+        </div>
+    );
+}; 
 
 const App = () => {
     const colors = [ '#518cef', '#3b10c4', '#9aede6', '#8ee763', '#a30dd0' ];
